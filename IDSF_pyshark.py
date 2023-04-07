c419486c489eadcf574f1adcc6512a7aba360818
NLag <NLag@users.noreply.github.com>
2023-04-07 03:04:59 +0200

    update IDSF capture data

--- IDSF_pyshark.py
+++ IDSF_pyshark.py
@@ -1,27 +1,37 @@
 import pyshark
 import pandas as pd
-import time
+import time 
 import traceback
 import signal
 from datetime import datetime
-import sys
+import sys, os
 from threading import Thread
+import threading
+from collections import deque
+import csv
 
 ###############################################################################
 #dataset folder
-modelfolder = './models/'
+modelfolder = './captured_traffic/'
 
 #feature to extract
-featurenamefile = modelfolder + 'Feature_name.dat'
+featurenamefile = 'Feature_name.dat'
 with open(featurenamefile) as file:
     feature_name = [line.rstrip() for line in file]
 file.close()
-
+ft_dict = {key: i for i, key in enumerate(feature_name)}
 n_feature = len(feature_name)
-dataset_df = pd.DataFrame(columns=feature_name)
+
+# dataset_df = pd.DataFrame(columns=feature_name)
+dataset_deque = deque([])
+has_data_event = threading.Event()
 
 dtime_str = datetime.now().strftime('%d-%m-%Y_%H%M')
 csvfile = modelfolder + 'captured_dataset_'+dtime_str+'.csv'
+
+#stop signal and stop ack
+STOP_CAPTURE = False
+STOP_OUTPUT = False
 
 ###############################################################################
 
@@ -155,41 +165,227 @@ def packet_to_dataframe(pk,time_delta,time_rela):
         traceback.print_exc()
     return merge_df
 
-def packet_to_list(pk,time_start,time_previous):
+#packet to list
+def packet_to_lists(pk,time_start,time_previous,time_now):
+    '''process packet and put to queue'''
     global feature_name
     global n_feature
-    now = time.time()
-    pk_lst = [None] * n_feature
+    global dataset_deque
+    global has_data_event
+    pk_lst = []
+    ft_lst = [None] * n_feature
+    ft_lst[ft_dict['frame.time_delta']]=time_now-time_previous
+    ft_lst[ft_dict['frame.time_relative']]=time_now-time_start
+    #IP
+    #['version', 'hdr_len', 'dsfield', 'dsfield_dscp', 'dsfield_ecn', 'len', 'id', 'flags', 
+    # 'flags_rb', 'flags_df', 'flags_mf', 'frag_offset', 'ttl', 'proto', 'checksum', 'checksum_status', 
+    # 'src', 'addr', 'src_host', 'host', 'dst', 'dst_host']
+    #pk.ip.version.int_value
+    ft_lst[ft_dict['ip.len']]=pk.ip.len.hex_value
+    ft_lst[ft_dict['ip.protocol']]=pk.ip.proto.hex_value
+    ft_lst[ft_dict['ip.src']]=pk.ip.src.show
+    ft_lst[ft_dict['ip.dst']]=pk.ip.dst.show
+    #TCP
+    #['srcport', 'dstport', 'port', 'stream', 'len', 'seq', 'seq_raw', 'nxtseq', 'ack', 'ack_raw',
+    #  'hdr_len', 'flags', 'flags_res', 'flags_ns', 'flags_cwr', 'flags_ecn', 'flags_urg', 'flags_ack', 
+    #  'flags_push', 'flags_reset', 'flags_syn', 'flags_fin', 'flags_str', 'window_size_value', 'window_size',
+    #  'window_size_scalefactor', 'checksum', 'checksum_status', 'urgent_pointer', 'options', 'options_nop', 
+    #  'option_kind', 'options_timestamp', 'option_len', 'options_timestamp_tsval', 'options_timestamp_tsecr',
+    #  'analysis', 'analysis_acks_frame', 'analysis_ack_rtt', '', 'time_relative', 'time_delta']
+    ft_lst[ft_dict['tcp.srcport']]=pk.tcp.srcport.hex_value
+    ft_lst[ft_dict['tcp.dstport']]=pk.tcp.dstport.hex_value
+    # ft_lst[ft_dict['tcp.seglen']]=int(pk.tcp.len)
+    # ft_lst[ft_dict['tcp.seg_raw']]=int(pk.tcp.seq_raw)
+    # ft_lst[ft_dict['tcp.ack_raw']]=int(pk.tcp.ack_raw)
+    ft_lst[ft_dict['tcp.time_relative']]=float(pk.tcp.time_relative)
+    ft_lst[ft_dict['tcp.time_delta']]=float(pk.tcp.time_delta)
+    if ("MQTT" not in pk):
+        dataset_deque.append(ft_lst)
+        return 0
+    
+    #MQTT
+    # ['hdrflags', 'msgtype', 'len']
+    mqtt_layers = pk.get_multiple_layers('mqtt')
+    mqtt_count=0
+    for mqtt in mqtt_layers:
+        new_ft_lst = ft_lst.copy()
+        mqtt_count+=1
+        new_ft_lst[ft_dict['mqtt.num']]=mqtt_count
+        new_ft_lst[ft_dict['mqtt.hdrflags']]=mqtt.hdrflags.hex_value
+        new_ft_lst[ft_dict['mqtt.msgtype']]=int(mqtt.msgtype)
+        new_ft_lst[ft_dict['mqtt.len']]=int(mqtt.len)
+        if new_ft_lst[ft_dict['mqtt.msgtype']] == 1:
+            #MQTT CONNECT
+            #['hdrflags', 'msgtype', 'hdr_reserved', 'len', 'proto_len', 'protoname', 'ver', 'conflags', 'conflag_uname', 
+            # 'conflag_passwd', 'conflag_retain', 'conflag_qos', 'conflag_willflag', 'conflag_cleansess', 'conflag_reserved', 
+            # 'kalive', 'clientid_len', 'clientid', 'willtopic_len', 'willtopic', 'willmsg_len', 'willmsg'
+            # , 'username_len', 'username', 'passwd_len', 'passwd']
+            new_ft_lst[ft_dict['mqtt.proto_len']]=int(mqtt.proto_len)
+            new_ft_lst[ft_dict['mqtt.protoname']]=str(mqtt.protoname)
+            new_ft_lst[ft_dict['mqtt.ver']]=int(mqtt.ver)
+            new_ft_lst[ft_dict['mqtt.conflags']]=mqtt.conflags.hex_value
+            new_ft_lst[ft_dict['mqtt.conflag.uname']]= int(mqtt.conflag_uname)
+            new_ft_lst[ft_dict['mqtt.conflag.passwd']]=int(mqtt.conflag_passwd)
+            new_ft_lst[ft_dict['mqtt.conflag.willretain']]=int(mqtt.conflag_retain)
+            new_ft_lst[ft_dict['mqtt.conflag.willqos']]=int(mqtt.conflag_qos)
+            new_ft_lst[ft_dict['mqtt.conflag.willflag']]=int(mqtt.conflag_willflag)
+            new_ft_lst[ft_dict['mqtt.conflag.cleansess']]=int(mqtt.conflag_cleansess)
+            new_ft_lst[ft_dict['mqtt.conflag.reserved']]=int(mqtt.conflag_reserved)
+            new_ft_lst[ft_dict['mqtt.kalive']]=mqtt.kalive.hex_value
+            new_ft_lst[ft_dict['mqtt.clientid_len']]=int(mqtt.clientid_len)
+            if new_ft_lst[ft_dict['mqtt.clientid_len']] > 0:
+                new_ft_lst[ft_dict['mqtt.clientid']] = str(mqtt.clientid)
+            if new_ft_lst[ft_dict['mqtt.conflag.willflag']] == 1:
+                new_ft_lst[ft_dict['mqtt.willtopic_len']]=int(mqtt.willtopic_len)
+                if new_ft_lst[ft_dict['mqtt.willtopic_len']]>0:
+                    new_ft_lst[ft_dict['mqtt.willtopic']]=str(mqtt.willtopic)
+                new_ft_lst[ft_dict['mqtt.willmsg_len']]=int(mqtt.willmsg_len)
+                if new_ft_lst[ft_dict['mqtt.willmsg_len']]>0:
+                    new_ft_lst[ft_dict['mqtt.willmsg']]=str(mqtt.willmsg)
+            if new_ft_lst[ft_dict['mqtt.conflag.uname']] == 1:
+                new_ft_lst[ft_dict['mqtt.username_len']]=int(mqtt.username_len)
+                if new_ft_lst[ft_dict['mqtt.username_len']]>0:
+                    new_ft_lst[ft_dict['mqtt.username']]=str(mqtt.username)
+            if new_ft_lst[ft_dict['mqtt.conflag.passwd']] == 1:
+                new_ft_lst[ft_dict['mqtt.passwd_len']]=int(mqtt.passwd_len)
+                if new_ft_lst[ft_dict['mqtt.passwd_len']]>0:
+                    new_ft_lst[ft_dict['mqtt.passwd']]=str(mqtt.passwd)
 
+        elif new_ft_lst[ft_dict['mqtt.msgtype']] == 2:
+            #MQTT CONACK
+            # ['hdrflags', 'msgtype', 'hdr_reserved', 'len', 'conack_flags', 'conack_flags_reserved', 
+            #  'conack_flags_sp', 'conack_val']
+            new_ft_lst[ft_dict['mqtt.conack.flags']] = mqtt.conack_flags.hex_value
+            new_ft_lst[ft_dict['mqtt.conact.flags.sp']] = int(mqtt.conack_flags_sp)
+            new_ft_lst[ft_dict['mqtt.conack.val']] = int(mqtt.conack_val)
+            
+        elif new_ft_lst[ft_dict['mqtt.msgtype']] == 3:
+            #MQTT PUBLISH
+            #['hdrflags', 'msgtype', 'dupflag', 'qos', 'retain', 'len', 'topic_len', 'topic', 'msgid', 'msg']
+            new_ft_lst[ft_dict['mqtt.dupflag']]= int(mqtt.dupflag)
+            new_ft_lst[ft_dict['mqtt.qos']]=int(mqtt.qos)
+            new_ft_lst[ft_dict['mqtt.retain']]=int(mqtt.retain)
+            new_ft_lst[ft_dict['mqtt.topic_len']]=int(mqtt.topic_len)
+            if new_ft_lst[ft_dict['mqtt.topic_len']] > 0:
+                new_ft_lst[ft_dict['mqtt.topic']]=str(mqtt.topic)
+            msgid_len = 0
+            if new_ft_lst[ft_dict['mqtt.qos']] != 0:
+                msgid_len = 2
+                new_ft_lst[ft_dict['mqtt.msgid']]=int(mqtt.msgid)
+            msglen = new_ft_lst[ft_dict['mqtt.len']] - 2 - new_ft_lst[ft_dict['mqtt.topic_len']] - msgid_len
+            if msglen > 0 :
+                new_ft_lst[ft_dict['mqtt.msglen']] = msglen
+                new_ft_lst[ft_dict['mqtt.msg']] = str(mqtt.msg)
+            else:
+                new_ft_lst[ft_dict['mqtt.msglen']] = 0
+            
+        elif new_ft_lst[ft_dict['mqtt.msgtype']] in [4,5,6,7,11]:
+            #MQTT PUBACK, PUBREC, PUBREL, PUBCOMP, UNSUBACK
+            #['hdrflags', 'msgtype', 'hdr_reserved', 'len', 'msgid']
+            new_ft_lst[ft_dict['mqtt.msgid']]=int(mqtt.msgid)
 
+        elif new_ft_lst[ft_dict['mqtt.msgtype']] == 8:
+            #MQTT SUBSCRIBE
+            #['hdrflags', 'msgtype', 'hdr_reserved', 'len', 'msgid', 'topic_len', 'topic', 'sub_qos']
+            new_ft_lst[ft_dict['mqtt.msgid']]=int(mqtt.msgid)
+            new_ft_lst[ft_dict['mqtt.topic_len']]=int(mqtt.topic_len)
+            if new_ft_lst[ft_dict['mqtt.topic_len']] > 0:
+                new_ft_lst[ft_dict['mqtt.topic']]=str(mqtt.topic)
+            new_ft_lst[ft_dict['mqtt.sub.qos']] = int(mqtt.sub_qos)
+            
+        elif new_ft_lst[ft_dict['mqtt.msgtype']] == 9:
+            #MQTT SUBACK
+            #['hdrflags', 'msgtype', 'hdr_reserved', 'len', 'msgid', 'suback_qos']
+            new_ft_lst[ft_dict['mqtt.msgid']]=int(mqtt.msgid)
+            new_ft_lst[ft_dict['mqtt.suback.qos']] = int(mqtt.suback_qos)
+
+        elif new_ft_lst[ft_dict['mqtt.msgtype']] == 10:
+            print(mqtt.field_names)
+            #MQTT UNSUBSCRIBE       
+            # ['hdrflags', 'msgtype', 'hdr_reserved', 'len', 'msgid', 'topic_len', 'topic']
+            new_ft_lst[ft_dict['mqtt.msgid']]=int(mqtt.msgid)
+            new_ft_lst[ft_dict['mqtt.topic_len']]=int(mqtt.topic_len)
+            if new_ft_lst[ft_dict['mqtt.topic_len']] > 0:
+                new_ft_lst[ft_dict['mqtt.topic']]=str(mqtt.topic)
+    
+        # elif new_ft_lst[ft_dict['mqtt.msgtype']] in [12,13,14]:
+            #MQTT PINGREQ, PINGRESP, DISCONNECT
+        # else:
+        #     #pk_lstMQTT Reserved
+        pk_lst.append(new_ft_lst)
+    dataset_deque.extend(pk_lst)
+    has_data_event.set()
+
+###############################################################################
+
+def signal_handler(*args):
+    cur_thrd = threading.current_thread()
+    if cur_thrd.daemon == False:
+        #if main thread, stop capture
+        global STOP_CAPTURE
+        STOP_CAPTURE = True
+
+        print('wait pk_thread to finish')
+        for thrd in threading.enumerate():
+            if thrd.daemon == True and thrd.name=='FeT':
+                thrd.join()
+        
+        global STOP_OUTPUT
+        STOP_OUTPUT=True
+        global has_data_event
+        has_data_event.set()
+        print('wait output thread to finish')
+        global out_thrd
+        out_thrd.join()
+
+        # dump data if exist
+        if len(dataset_deque) > 0:
+            global csvfile
+            n = len(dataset_deque)
+            rows = [dataset_deque.popleft() for i in range(n)]
+            with open(csvfile,'a') as f:
+                writer = csv.writer(f)
+                writer.writerows(rows)
+            print('dumped last rows',n)
+
+# signal.signal(signal.SIGTERM, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)
+
+###############################################################################
+
+def write_queue_to_file(filename):
+    global dataset_deque
+    global has_data_event
+    while STOP_OUTPUT==False:
+        has_data_event.wait()
+        n = len(dataset_deque)
+        rows = [dataset_deque.popleft() for i in range(n)]
+        has_data_event.clear()
+        with open(filename,'a') as f:
+            writer = csv.writer(f)
+            writer.writerows(rows)
 
 #capture TCP
 def capture_live_packets(network_interface):
-    global dataset_df
+    '''capture live packet and create thread to process packet'''
+    global STOP_CAPTURE
     capture = pyshark.LiveCapture(interface=network_interface,display_filter='tcp')
-    start_time = time.time()
+    now = time.time()
+    start_time = now
     lastpk_time = start_time
     for raw_packet in capture.sniff_continuously():
-        # if "MQTT" in raw_packet:
-        #     print(raw_packet)
-        now = time.time()
-        pk_relative_time = (now - start_time) % 1000
-        pkduration = now - lastpk_time
         lastpk_time = now
-        df_packet = packet_to_dataframe(raw_packet,pkduration,pk_relative_time)
-        dataset_df = pd.concat([dataset_df,df_packet],ignore_index=True)
-        # print(df_packet)
+        now = time.time()
+        if STOP_CAPTURE == False:
+            pk_thrd = Thread(target=packet_to_lists,args=(raw_packet,start_time,lastpk_time,now),daemon=True,name='FeT')
+            pk_thrd.start()
+        else:
+            break
 
-        # res = AImodel_Detect_Abnormal(df_packet,label, model_dict)
+try:
+    out_thrd = Thread(target=write_queue_to_file,args=(csvfile,),daemon=True,name='Output')
+    out_thrd.start()
 
-def signal_handler(*args):
-    print('Dumping data:', csvfile )
-    dataset_df.to_csv(csvfile,index=False,columns=feature_name)
-    print('Dumped data before exit')
-    sys.exit(0)
-
-
-signal.signal(signal.SIGTERM, signal_handler)
-signal.signal(signal.SIGINT, signal_handler)
-
-capture_live_packets('ogstun')
+    capture_live_packets('ogstun')
+except:
+    traceback.print_exc()
